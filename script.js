8022aca551614c1cc55258bfd5c3a491746585ad
mdjunaid02 <156643108+mdjunaid02@users.noreply.github.com>
2025-12-09 19:10:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const table=document.querySelector("table");
+	const prices=document.querySelectorAll(".price");
+
+	let sum=0;
+
+	prices.forEach(cell =>{
+		sum+=Number(cell.innerText);
+	});
+
+	const oldRow=document.getElementById("totalRow");
+	if(oldRow) oldRow.remove();
+
+	const tr=document.createElement("tr");
+	tr.id="totalRow";
+
+	const td=document.createElement("td");
+	td.colSpan=2;
+	td.innerText=sum;
+
+	tr.appendChild(td);
+	table.appendChild(tr);
   
 };
 
